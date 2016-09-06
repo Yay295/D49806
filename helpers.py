8730224f10ac9dd175cde4ed899f9183b696638d
Yay295 <Yay295@users.noreply.github.com>
2016-09-06 17:52:10 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,13 +1,40 @@
-import sys, glob
+import glob, os, pathlib, time
 
-def indexOf(element, list):
-	try: return list.index(element)
-	except ValueError: return False
 
-fileargloc = indexOf("-r", sys.argv);
-if fileargloc != False:
-	fileargloc = indexOf("--replace", sys.argv);
-files = {"start": sys.argv[fileargloc+2], "end": sys.argv[fileargloc+3]}
+def getFiles(arg):
+	if os.path.isFile(arg):
+		return [arg]
+	else:
+		files = glob.glob(arg)
+		if len(files) == 0:
+			print(file, 'was not found')
+			return False
+		else:
+			return files
 
-print(glob.glob(sys.argv[0]))
-print(files)
+def deleteFiles(files):
+	for file in files:
+		try:
+			os.remove(file)
+		except OSError as e:
+			print(e)
+
+def touchFiles(files):
+	for file in files:
+		try:
+			pathlib.Path(file).touch()
+		except FileExistsError as e:
+			print(e)
+
+
+def parseDate(string):
+	try:
+		return time.strptime(string, '%d%m%Y')
+	except ValueError as e:
+		print e
+
+def parseTime(string):
+	try:
+		return time.strptime(string, '%H%M%S')
+	except ValueError as e:
+		print e
