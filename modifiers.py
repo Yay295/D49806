736669c01310917fb669bdfc0ab26b125c892341
Yay295 <Yay295@users.noreply.github.com>
2016-09-08 14:23:20 -0600

    add date, time, touch, and delete
    though date and time aren't finished

--- modifiers.py
+++ modifiers.py
@@ -1,10 +1,41 @@
-import re
+import os, re
+
+
+def changeDate(files, date):
+	for file in files:
+		continue
+
+def changeTime(files, time):
+	for file in files:
+		continue
+
+
+def touchFiles(files):
+	''' man touch '''
+	for file in files:
+		try:
+			pathlib.Path(file).touch()
+		except FileExistsError as e:
+			print(e)
+
+def deleteFiles(files):
+	''' Attempts to delete the files passed in as a list of filenames. '''
+	for file in files:
+		try:
+			os.remove(file)
+		except OSError as e:
+			print(e)
+
 
 modify = {
 	'upper': lambda files: list(map(lambda filename: filename.upper(), files)),
 	'lower': lambda files: list(map(lambda filename: filename.lower(), files)),
 	'trim': lambda files, amount: list(map(lambda filename: filename[amount if amount >= 0 else 0 : len(filename) if amount >= 0 else len(filename) + amount], files)),
-	'replace': lambda files, before, after: list(map(lambda filename: re.sub(before, after, filename), files))
+	'replace': lambda files, before, after: list(map(lambda filename: re.sub(before, after, filename), files)),
+	'date': changeDate,
+	'time': changeTime,
+	'touch': touchFiles,
+	'delete': deleteFiles
 }
 
 # Code for Testing
