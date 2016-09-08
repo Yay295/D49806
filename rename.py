76fbf6a4a2f9b7a0bfd2d2dc4ba0f6b7439c77c2
Jessi Thompson <jessica.thompson@mines.sdsmt.edu>
2016-09-08 00:13:00 -0600

    Added notes - go over these on 9/8 meeting

--- rename.py
+++ rename.py
@@ -25,6 +25,21 @@ def main( argv ):
     ''' Starts program, checks for correct usage, checks platform, sets global variables
     and begins analysis of files in directory.'''
     
+    '''NOTES:
+    We can add an action='append' to the attributes of the parser.add_argument to make repeat 
+    instances of arguments save to lists of lists.
+    
+    For example:
+    -r a b -r c d
+    would be saved
+    args.replace = [[a, b], [c, d]]
+    
+    if we think we want that?
+    '''
+    
+    
+    ''' To do: Figure out how to call functions from other modules'''
+    
     parser = argparse.ArgumentParser( usage = "-h for help, -v for verbose, -i for int, -f for float" )
     
     # optional switches (may occur in any order)
@@ -44,22 +59,37 @@ def main( argv ):
     parser.add_argument('-t', '--trim', type=int, help='n > 0: trim n characters from start of each filename. n < 0: trim n characters from the end of each file name')
     
     # replace argument optional, but has required two strings at the end 
-    parser.add_argument('-r', '--replace', default = [], nargs='+') # replace accepts two string arguments in a list
+    parser.add_argument('-r', '--replace', action='append', default = [], nargs='+') # replace accepts two string arguments in a list
     
     # number argument optional, but has required two strings at the end
     parser.add_argument('-n', '--number', default = [], nargs='+')  # number accepts two string arguments in a list
     
     # parse command arguments
     args = parser.parse_args()
-    
+        
     # Check platform, if not Linux or Windows return 1 from program.
+    # pass systemPlatform into funcs that need to differentiate if necessary
     systemPlatform = setPlatform(platform.system())
     if systemPlatform == 'E':
         print('Platform not recognized by file rename tool. Exiting...')
         return 1
         
     # Parse command line arguments
+    '''
+     The idea here is to chop up the command line arguments after it encounters a -r, -t, or -n 
+    because that would end a sequence of changes - since we're executing consecutive arguments.
+
+    Some special cases...
+    In the case of encountering a Delete in a sequence of actions, stop execution there and delete the files listed after the delete
+    In the case of encountering a Print in a sequence of actions, stop execution there and just print the file names listed after the print
+    '''
     
+    '''
+    Once we have lists of commands like
+    [[Replace, 'string1', 'string2'], [trim, 2], [Upper]]
+    
+    iterate through the list and pass in the arguments to appropriate functions
+    '''
         
         
     
