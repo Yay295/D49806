0fe876b67f681cc7a3adafe4b9d3a614d6a06db2
Jessi Thompson <jessica.thompson@mines.sdsmt.edu>
2016-09-07 23:19:11 -0600

    Added argparse stuff and some documentation
    Added prelim argparse items, along with beginning of the program
    documentation. TODO - still need to put argeparse stuff in another file,
    and to parse the args into separate strings after it encounters the end
    of a list of strings

--- rename.py
+++ rename.py
@@ -1,30 +1,66 @@
+"""/*=====================================================================
+ Program: Program 1
+ Author: John Colton, Jessi Thompson
+ Class: CSC 461 Programming Languages
+ Instructor: Dr. Weiss
+ Date: September 22, 2016
+ Description: TODO: FILL IN
+ Input: 
+ Output:
+ Compilation instructions:
+ Usage:
+ Modifications: TODO: FILL IN FROM GITHUB COMMITS
+ Date                Comment
+ September 6         Look at parsing arguments
+ September 7         Set argparse, began to parse arguments into lists
+ 
+"""
 import sys, glob, platform, argparse, getopt
-
-# Using a "main" function in Python
-# CSC461 Programming Languages, Fall 2016 (JMW)
-# Main file for Program 1
 
 #Global variables
 platform
-helpToggle = False
-verboseToggle = False
-printToggle = False
-interactiveToggle = False
 
 # here is the "main" function, must define it before using it
 def main( argv ):
     ''' Starts program, checks for correct usage, checks platform, sets global variables
     and begins analysis of files in directory.'''
     
-    # check for correct number of command line arguments, parse parameters
-    parser = argparse.ArgumentParser(description='Parse the command line arguments!')
-    parser.add_argument('-l', '--lower')
-    parser.add_argument('-u', '--upper')
+    parser = argparse.ArgumentParser( usage = "-h for help, -v for verbose, -i for int, -f for float" )
     
-    parser.add_argument('-v', '--verbose', help='print old and new filenames during processing', action = "store_true")
-    parser.add_argument('-i', '--interactive', help='interactive mode, ask user prior to processing each file')
-    parser.add_argument('-d', '--delete', help= 'delete files')
-    parser.add_argument('-dt', '--touch', help = 'update date/time stamp to current date/time')
+    # optional switches (may occur in any order)
+    parser.add_argument('-v', "--verbose", action="store_true", help="print old and new filenames during processing")
+    parser.add_argument('-p', "--print", action="store_true", help="only print old and new filenames, do not rename")
+    parser.add_argument('-i', "--interactive", action="store_true", help="interactive mode, ask user prior to processing each file")
+    
+    # just some optional arguments
+    parser.add_argument('-l', '--lower', help='convert filenames to lowercase')
+    parser.add_argument('-u', '--upper', help='convert filenames to uppercase')
+    parser.add_argument('-d', '--delete', help='delete files')
+    parser.add_argument('-dt', '--touch', help='"touch files" (update time/date stamp to current date/time)')
+    parser.add_argument('-D', '--date', type=str, help='format: -D/--date DDMMYYYY change file datestamps')
+    parser.add_argument('-T', '--time', type=str, help='format: -T/--time HHMMSS change file timestamps')
+    
+    # trim argument optional, but required positional argument afteward
+    parser.add_argument('-t', '--trim', type=int, help='n > 0: trim n characters from start of each filename. n < 0: trim n characters from the end of each file name')
+    
+    # replace argument optional, but has required two strings at the end 
+    parser.add_argument('-r', '--replace', default = [], nargs='+') # replace accepts two string arguments in a list
+    
+    # number argument optional, but has required two strings at the end
+    parser.add_argument('-n', '--number', default = [], nargs='+')  # number accepts two string arguments in a list
+    
+    # parse command arguments
+    args = parser.parse_args()
+
+   # #   # print results of parsing
+   #  print( 'command line args:', sys.argv )
+   #  print( 'argparse:', args )
+   #  print( 'args.verbose =', args.verbose )
+   #  print( 'args.print =', args.print )
+   #  print( 'args.interactive =', args.interactive )
+   #  print( 'args.trim =', args.trim)
+   #  print( 'args.replace =', args.replace)
+   #  print( 'args.number =', args.number)
     
     # Check platform, if not Linux or Windows return.
     if setPlatform(platform.system()) != 0:
