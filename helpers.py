c7be3e73a58c0e0da94f9e90e143244457bfc55b
Yay295 <Yay295@users.noreply.github.com>
2016-09-08 14:21:51 -0600

    move touch and delete to modifiers.py

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
 import glob, os, pathlib, time
 
-
 def getFiles(arg):
 	''' Checks that the file(s) passed as a string actually exist, and returns it/them as a list. '''
 	if os.path.isFile(arg):
@@ -11,23 +10,6 @@ def getFiles(arg):
 			return files
 		else:
 			print(file, 'was not found')
-
-def deleteFiles(files):
-	''' Attempts to delete the files passed in as a list of filenames. '''
-	for file in files:
-		try:
-			os.remove(file)
-		except OSError as e:
-			print(e)
-
-def touchFiles(files):
-	''' man touch '''
-	for file in files:
-		try:
-			pathlib.Path(file).touch()
-		except FileExistsError as e:
-			print(e)
-
 
 def parseDate(string):
 	''' Converts a string in the format DDMMYYYY to a format used by the `time` library. '''
