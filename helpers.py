74d48455d257755322e439e97475dcd0694ed6ea
Yay295 <Yay295@users.noreply.github.com>
2016-09-12 11:07:44 -0600

    formatting and documentation

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
-import glob, os, pathlib, time
+import glob, os, time
+
 
 def getFiles(arg):
 	''' Checks that the file(s) passed as a string actually exist, and returns it/them as a list. '''
@@ -10,6 +11,8 @@ def getFiles(arg):
 			return files
 		else:
 			print(file, 'was not found')
+			return []
+
 
 def parseDate(string):
 	''' Converts a string in the format DDMMYYYY to a format used by the `time` library. '''
@@ -27,17 +30,18 @@ def parseTime(string):
 
 
 def setPlatform(val):
-	'''Checks whether the machine is running Linux or Windows. If it's not running
-	Linux or Windows, it throws an error and returns 1'''
+	''' Checks whether the machine is running Linux or Windows. If it's not running
+	Linux or Windows, it returns 'E' for "Error". '''
 	if val == 'Linux':
 		return 'L'
 	elif val == 'Windows':
 		return 'W'
 	else:
-		return 'E'  # for error
-		
-		
-def makeList(argv, args):		
+		return 'E'
+
+
+def makeList(argv, args):
+	''' Converts the arguments given to this program into a formatted list for easier execution. '''
 	masterExecutionList = []
 	# set some important variables...
 	replaceCount = 0
@@ -45,42 +49,42 @@ def makeList(argv, args):
 	trimCount = 0
 	dateCount = 0
 	timeCount = 0
-	
+
 	# loop through sys.argv, building a list of execution commands, tupled with their parameters
 	for cmd in argv:
 		if cmd in ('-l', '--lower'):
-			masterExecutionList.append(('lower', None))      
-			
+			masterExecutionList.append(('lower', None))
+
 		elif cmd in ('-u', '--upper'):
 			masterExecutionList.append(('upper', None))
-			
+
 		elif cmd in ('-p', '--print'):
 			masterExecutionList.append(('print', None))
-			
+
 		elif cmd in ('-d', '--delete'):
 			masterExecutionList.append(('delete', None))
-		
+
 		elif cmd in ('-dt', '--touch'):
 			masterExecutionList.append(('touch', None))
-			
+
 		elif cmd in ('-t', '--trim'):
 			masterExecutionList.append(('trim', args.trim[trimCount]))
 			trimCount += 1
-			
+
 		elif cmd in ('-r', '--replace'):
 			masterExecutionList.append(('replace', args.replace[replaceCount])) # append execution list with tuple
-			replaceCount += 1    # keep track of what repeat we've found to do lookup
-	
+			replaceCount += 1 # keep track of what repeat we've found to do lookup
+
 		elif cmd in ('-n', '--number'):
 			masterExecutionList.append(('number', args.number[numberCount]))
 			numberCount += 1
-			
+
 		elif cmd in ('-D', '--date'):
 			masterExecutionList.append(('date', args.date[dateCount]))
 			dateCount += 1
-		
+
 		elif cmd in ('-T', '--time'):
 			masterExecutionList.append(('time', args.time[timeCount]))
 			timeCount += 1
-		
+
 	return masterExecutionList
