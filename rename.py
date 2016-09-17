8335ed2bab4ff1b41dd3d9d80afce7c2c1afe49f
Yay295 <Yay295@users.noreply.github.com>
2016-09-17 16:42:03 -0600

    " -> '

--- rename.py
+++ rename.py
@@ -29,25 +29,25 @@
  September 13	Bug fixes and file management added.
  September 14	`changeDate` and `changeTime` functions finished.
  September 16	Cleaned up code and updated main documentation.
+ September 17	More formatting and cleaning.
 '''
 
-import sys, glob, platform, argparse, getopt
+import sys, platform, argparse
 
-# here is the "main" function, must define it before using it
 def main(argv):
     ''' Starts program, checks for correct usage, checks platform, sets global
 	variables, and begins analysis of files in directory. '''
 
-    parser = argparse.ArgumentParser(usage = "-h or --help for full help and usage menu")
+    parser = argparse.ArgumentParser(usage = '-h or --help for full help and usage menu')
 
     # plain arguments
-    parser.add_argument('-v', "--verbose", action="store_true", help="print old and new filenames during processing")
-    parser.add_argument('-p', "--print", action="store_true", help="only print old and new filenames, do not rename")
-    parser.add_argument('-i', "--interactive", action="store_true", help="interactive mode, ask user prior to processing each file")
-    parser.add_argument('-l', '--lower', action="store_true", help='convert filenames to lowercase')
-    parser.add_argument('-u', '--upper', action="store_true", help='convert filenames to uppercase')
-    parser.add_argument('-d', '--delete', action="store_true", help='delete files')
-    parser.add_argument('-dt', '--touch', action="store_true", help='"touch files" (update time/date stamp to current date/time)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='print old and new filenames during processing')
+    parser.add_argument('-p', '--print', action='store_true', help='only print old and new filenames, do not rename')
+    parser.add_argument('-i', '--interactive', action='store_true', help='interactive mode, ask user prior to processing each file')
+    parser.add_argument('-l', '--lower', action='store_true', help='convert filenames to lowercase')
+    parser.add_argument('-u', '--upper', action='store_true', help='convert filenames to uppercase')
+    parser.add_argument('-d', '--delete', action='store_true', help='delete files')
+    parser.add_argument('-dt', '--touch', action='store_true', help=''touch files' (update time/date stamp to current date/time)')
     # arguments with required parameters and potential consecutive calls with their own data
     parser.add_argument('-t', '--trim', type=int, action='append', default=[], help='n > 0: trim n characters from start of each filename. n < 0: trim n characters from the end of each file name')
     parser.add_argument('-r', '--replace', action='append', nargs=2)
@@ -55,7 +55,7 @@ def main(argv):
     parser.add_argument('-D', '--date', action='append', nargs=1, metavar='DDMMYYYY', type=str, help='change file datestamps')
     parser.add_argument('-T', '--time', action='append', nargs=1, metavar='HHMMSS', type=str, help='change file timestamps')
     # Required arguments - list of filenames
-    parser.add_argument("files", type=str, nargs='+', help="list of files to be modified")
+    parser.add_argument('files', type=str, nargs='+', help='list of files to be modified')
 
     # parse command arguments
     args = parser.parse_args()
